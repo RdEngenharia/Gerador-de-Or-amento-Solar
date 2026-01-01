761680c7417bf545299e1eb53b8a7a7bb1a5dd7c
RdEngenharia <rodrigues.solar@hotmail.com>
2026-01-01 16:53:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADICIONADO: Define a base do caminho para o GitHub Pages
+      base: '/Gerador-de-Or-amento-Solar/',
       server: {
         port: 3000,
         host: '0.0.0.0',
