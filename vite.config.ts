c360cdb8f99ae874d1c5cce0748ed5312ec35c71
RdEngenharia <rodrigues.solar@hotmail.com>
2026-01-01 17:08:04 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,18 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      // ADICIONADO: Define a base do caminho para o GitHub Pages
-      base: '/Gerador-de-Or-amento-Solar/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANTE: Isso garante que o site saiba que está na subpasta do GitHub
+  base: './', 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 });
