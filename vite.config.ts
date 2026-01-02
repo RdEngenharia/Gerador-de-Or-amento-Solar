6938dc53a9eb2e0f9bd68ee974b5af4ac9f31df3
RdEngenharia <rodrigues.solar@hotmail.com>
2026-01-01 22:38:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { VitePWA } from 'vite-plugin-pwa'; // Adicione este plugin
 
 export default defineConfig({
-  // Mude de './' para o caminho exato do repositório
+  plugins: [react()],
+  // IMPORTANTE: Mudamos de './' para o caminho real da subpasta
   base: '/Gerador-de-Or-amento-Solar/', 
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: false, // Ele vai usar o seu manifest.json da pasta public
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'], // Arquivos que ele vai salvar para funcionar OFFLINE
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => url.origin === self.location.origin,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'static-assets',
-            },
-          },
-        ],
-      },
-    })
-  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
